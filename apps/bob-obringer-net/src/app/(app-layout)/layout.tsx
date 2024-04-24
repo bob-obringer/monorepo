3072b1c50912572102e2e1e6a2488c04802e887b
Bob Obringer <bob@obringer.net>
2024-04-24 16:36:23 -0400

    fixed iphone layout

--- apps/bob-obringer-net/src/app/(app-layout)/layout.tsx
+++ apps/bob-obringer-net/src/app/(app-layout)/layout.tsx
@@ -7,12 +7,14 @@ const alertMessage = "";
 
 export default function AppLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="overflow-hidden">
-      {alertMessage && <Alert>{alertMessage}</Alert>}
-      <AppHeader className={cx(alertMessage ? "mt-10" : "mt-0")} />
-      <div className="overflow-hidden pb-28 md:pb-40">{children}</div>
+    <>
+      <div>
+        {alertMessage && <Alert>{alertMessage}</Alert>}
+        <AppHeader className={cx(alertMessage ? "mt-10" : "mt-0")} />
+        <div className="pb-28 md:pb-40">{children}</div>
+      </div>
       <AppFooter />
-    </div>
+    </>
   );
 }
 
