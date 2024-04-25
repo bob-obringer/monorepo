501972d78f20e3ae386615b8ffc329a37be0aea0
Bob Obringer <bob@obringer.net>
2024-04-25 13:59:06 -0400

    added analytics

--- apps/bob-obringer-net/src/app/layout.tsx
+++ apps/bob-obringer-net/src/app/layout.tsx
@@ -4,6 +4,8 @@ import { ReactNode } from "react";
 import { BobObringerAiProvider } from "@/features/ai/bob-obringer-ai-context";
 import { bobObringerFontClasses } from "@bob-obringer/nextjs-fonts";
 import { UiContextProvider } from "@/features/ui/ui-context";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Bob Obringer",
@@ -17,6 +19,8 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <UiContextProvider>
           <BobObringerAiProvider>{children}</BobObringerAiProvider>
         </UiContextProvider>
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
