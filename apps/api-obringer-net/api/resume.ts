52c8a8b788dd33abdbc5adc0f36ceb653b607c5a
Bob Obringer <bob@obringer.net>
2024-05-23 14:22:17 -0400

    chore: fixed api fonts

---
+++ apps/api-obringer-net/api/resume.ts
@@ -0,0 +1,123 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import PDFDocument from "pdfkit";
+import {
+  getHomepage,
+  getResumeCompanies,
+} from "./sanity-io/queries/resume-company.js";
+
+import { ArgentumNovusMedium } from "./fonts/ArgentumNovus-Medium.js";
+import { ArgentumNovusRegular } from "./fonts/ArgentumNovus-Regular.js";
+import { YsabeauSCBold } from "./fonts/YsabeauSC-Bold.js";
+
+function inch(n: number) {
+  return n * 72;
+}
+
+const margin = inch(0.75);
+
+export default async function handler(
+  _req: VercelRequest,
+  res: VercelResponse,
+) {
+  const doc = new PDFDocument({
+    size: "LETTER",
+    margin,
+  });
+
+  doc.registerFont("heading", Buffer.from(YsabeauSCBold, "base64"));
+  doc.registerFont("title", Buffer.from(ArgentumNovusMedium, "base64"));
+  doc.registerFont("body", Buffer.from(ArgentumNovusRegular, "base64"));
+
+  const maxPageBodyHeight = doc.page.height - margin * 2;
+
+  // Contact Info
+  doc
+    .font("body", 12)
+    .fillColor([40, 112, 189])
+    .text("bob.obringer.net", margin, margin, {
+      align: "right",
+      baseline: "hanging",
+      lineGap: 4,
+    })
+    .text("bob@obringer.net", {
+      align: "right",
+      baseline: "hanging",
+      lineGap: 4,
+    })
+    .fillColor("black")
+    .text("917-656-1685", {
+      align: "right",
+      baseline: "hanging",
+    });
+
+  // Name and Title
+  doc
+    .font("heading", 42)
+    .text("Bob Obringer", margin, margin, { baseline: "hanging" })
+    .moveUp(0.2)
+    .font("heading", 24)
+    .text("Product Architect", {
+      baseline: "hanging",
+    });
+
+  // get resume
+  const homepage = await getHomepage();
+  doc.moveDown(1);
+  doc.font("body", 14).text(homepage.bio, { lineGap: 8 });
+
+  const companies = await getResumeCompanies();
+
+  doc.addPage();
+
+  for (const company of companies) {
+    if (doc.y !== margin) {
+      const titleHeight = doc.heightOfString(company.name ?? "", {
+        lineGap: 4,
+      });
+      const positionHeight = doc.heightOfString(company.position ?? "", {
+        lineGap: 8,
+      });
+      const highlightHeight = doc.heightOfString(
+        company.highlights?.map((h) => h.text).join() ?? "",
+        {
+          lineGap: 2,
+          paragraphGap: 4,
+        },
+      );
+
+      const companyBottom =
+        doc.y + titleHeight + positionHeight + highlightHeight;
+
+      if (companyBottom > maxPageBodyHeight) {
+        doc.addPage();
+      }
+    }
+
+    doc
+      .font("heading", 24)
+      .text(company.name ?? "", { lineGap: 4 })
+      .font("title", 14)
+      .text(company.position ?? "", { lineGap: 8 })
+      .font("body", 12)
+      .list(company.highlights?.map((highlight) => highlight.text) ?? [], {
+        lineGap: 2,
+        paragraphGap: 4,
+        bulletRadius: 2,
+      })
+      .moveDown(1);
+  }
+
+  res.setHeader("Content-Type", "application/pdf");
+  res.setHeader(
+    "Content-Disposition",
+    'inline; filename="bob-obringer-resume.pdf"',
+  );
+  doc.pipe(res);
+  doc.end();
+}
+
+//
+// // const port = 3000;
+// // app.listen(port, () => {
+// //   return console.log(`Server is listening on ${port}`);
+// // });
--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -10,42 +10,4 @@ chroma.action(() => {
   console.log("not implemented");
 });
 
-// chroma
-//   .command("create <collection>")
-//   .description("Create a new collection")
-//   .action(async (collection) => {
-//     await getCollection(collection);
-//     console.log(`Collection ${collection} created successfully.`);
-//   });
-//
-// chroma
-//   .command("delete <collection>")
-//   .description("Delete a collection")
-//   .action(async (collection) => {
-//     await deleteCollection(collection);
-//     console.log(`Collection ${collection} deleted successfully.`);
-//   });
-//
-// chroma
-//   .command("peek <collection>")
-//   .description("Peek into a collection")
-//   .option("-l, --limit <number>", "Limit the number of results", "10")
-//   .action(async (collection, options) => {
-//     const c = await getCollection(collection);
-//     const r = await c.peek({ limit: parseInt(options.limit) });
-//     console.log(r);
-//   });
-//
-// chroma
-//   .command("query <collection> <text>")
-//   .description("Query a document")
-//   .action(async (collection, text) => {
-//     const c = await getCollection(collection);
-//     const results = await c.query({
-//       queryTexts: [text], // Chroma will embed this for you
-//       nResults: 1, // how many results to return
-//     });
-//     console.log(results);
-//   });
-
 program.parse(process.argv);
