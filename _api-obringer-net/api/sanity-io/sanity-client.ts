6822eb8ddcfd5081b836b78914153354f06a69e0
Bob Obringer <bob@obringer.net>
2024-05-23 12:41:24 -0400

    chore: added api

--- _api-obringer-net/api/sanity-io/sanity-client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@sanity/client";
-
-export const sanityClient = createClient({
-  projectId: "gkf7pxwm",
-  dataset: "production",
-  useCdn: false,
-  apiVersion: "2024-05-15",
-});
