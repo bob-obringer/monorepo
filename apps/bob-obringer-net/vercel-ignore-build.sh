281e5c1dc21c966bcd732c4b5afec4154e9ec85b
Bob Obringer <bob@obringer.net>
2024-06-04 11:17:32 -0400

    chore: testing skipping builds [skip-ci]

---
+++ apps/bob-obringer-net/vercel-ignore-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+npx turbo-ignore --fallback=HEAD^1
+if [ $? -eq 0 ]; then 
+  echo "No changes detected, skipping build."
+  exit 0
+fi
+
+git log -1 --pretty=oneline --abbrev-commit | grep -w "[skip ci]" 
+if [ $? -eq 0 ]; then
+  echo "Commit message contains '[skip ci]', skipping build."
+  exit 0
+else
+  echo "Running build..."
+  exit 1
+fi
