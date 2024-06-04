92a74280af707facfdaee74d028ed86ece44882c
Bob Obringer <bob@obringer.net>
2024-06-04 11:08:12 -0400

    chore: testing skipping builds [skip-ci]

--- apps/bob-obringer-net/src/app/opengraph-image.tsx
+++ apps/bob-obringer-net/src/app/opengraph-image.tsx
@@ -1,9 +1,7 @@
 import { OgImage } from "@/features/metadata/og/og-image";
 
 export const contentType = "image/png";
-
 export const alt = "Bob Obringer";
-
 export const size = {
   width: 1200,
   height: 630,
