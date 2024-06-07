b0e46a3252aa11fc33d9eab9c622c5952175f2ef
Bob Obringer <bob@obringer.net>
2024-06-07 12:46:59 -0400

    fix: added missing env vars from turbo config

--- apps/bob-obringer-net/src/app/(app-layout)/contact/opengraph-image.tsx
+++ apps/bob-obringer-net/src/app/(app-layout)/contact/opengraph-image.tsx
@@ -1,6 +1,5 @@
 import { OgImage } from "@/features/metadata/og/og-image";
 
-// eslint-disable-next-line @bob-obringer/next-prefer-named-exports
 export default async function Image() {
   return OgImage({ title: "Contact" });
 }
