96193ba109be5a3a3cb7bcd021988b4b0af63816
Bob Obringer <bob@obringer.net>
2024-06-08 18:35:55 -0400

    fix: fixed sorting on resume pdf

--- apps/api-obringer-net/api/_services/sanity-io/queries.ts
+++ apps/api-obringer-net/api/_services/sanity-io/queries.ts
@@ -74,6 +74,7 @@ const featuredResumeSkillQuery = groq`*[_type == "resumeSkill" && isFeatured == 
   orderRank,
   category->{
     name
+    orderRank
   }
 } | order(orderRank asc)`;
 
@@ -88,37 +89,37 @@ type Skill = {
   name: string;
   category: {
     name: string;
+    orderRank: string;
   };
 };
 
 export type CategorizedSkill = {
   name: string;
+  orderRank: string;
   skills: Set<string>;
 };
 
 export function getCategorizedSkills(
   skills: Array<Skill>,
 ): Array<CategorizedSkill> {
-  return skills.filter(isNotNull).reduce(
-    (acc, { name, category }) => {
+  return skills
+    .filter(isNotNull)
+    .reduce((acc, { name, category }) => {
       const categoryName = category.name;
 
       const existingCategory = acc.find((item) => item.name === categoryName);
       if (!existingCategory) {
         acc.push({
           name: categoryName,
+          orderRank: category.orderRank,
           skills: new Set([name]),
         });
       } else {
         existingCategory.skills.add(name);
       }
       return acc;
-    },
-    [] as Array<{
-      name: string;
-      skills: Set<string>;
-    }>,
-  );
+    }, [] as Array<CategorizedSkill>)
+    .sort((a, b) => a.orderRank.localeCompare(b.orderRank));
 }
 
 function isNotNull<T>(value: T): value is NonNullable<T> {
