7044e65d7b0f9c29adf46698ecc47fa6cc416dd5
Bob Obringer <bob@obringer.net>
2024-07-03 10:03:48 -0400

    fix: eslint plugin now allows default tailwind export

--- apps/bob-obringer-net/tailwind.config.ts
+++ apps/bob-obringer-net/tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 import { createTheme, componentsContent } from "@bob-obringer/components/theme";
 
-// eslint-disable-next-line @bob-obringer/next-prefer-named-exports
 export default {
   content: [componentsContent, "./src/**/*.{js,ts,jsx,tsx,mdx}"],
   plugins: createTheme(),
