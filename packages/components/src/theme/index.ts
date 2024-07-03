3230be01f5fd7cfeb7933e16a60c84aee90e35b6
Bob Obringer <bob@obringer.net>
2024-07-03 12:20:09 -0400

    feat: added ability to create entire tailwind config

--- packages/components/src/theme/index.ts
+++ packages/components/src/theme/index.ts
@@ -1,3 +1,23 @@
-export * from "./create-theme";
-export const componentsContent =
+import { createTheme, type ComponentsThemeConfig } from "./create-theme";
+import { type Config } from "tailwindcss";
+
+const componentsContent =
   "./node_modules/@bob-obringer/components/dist/components/**/*.{js,ts,jsx,tsx,mdx}";
+
+function createTailwindConfig({
+  contentFolders,
+  theme,
+}: {
+  contentFolders: Array<string>;
+  theme: ComponentsThemeConfig;
+}): Config {
+  return {
+    content: [
+      componentsContent,
+      ...contentFolders.map((folder) => `./${folder}/**/*.{js,ts,jsx,tsx,mdx}`),
+    ],
+    plugins: createTheme(theme),
+  };
+}
+
+export { createTheme, componentsContent, createTailwindConfig };
